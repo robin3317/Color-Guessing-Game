403a3d337af4ac9d30f613cdec4bd71f4e1a55b4
robin3317 <abdurrahmanrobin47@gmail.com>
2018-05-03 16:15:15 +0600

    all the sqaures are getting random colors, check the result according to user's interaction. And heading background also change according to clicked color, now its time to play again

--- index.js
+++ index.js
@@ -1,29 +1,55 @@
-let colors = [
-  "rgb(255, 0, 0)",
-  "rgb(255, 255, 0)",
-  "rgb(0, 255, 0)",
-  "rgb(0, 255, 255)",
-  "rgb(0, 0, 255)",
-  "rgb(255, 0, 255)"
-];
+let colors = generateRandomColors(6);
 //select all the square, see the console for better understand
 let squares = document.querySelectorAll(".square");
-let pickedColor = colors[3];
+let pickedColor = pickColor();
 let colorDisplay = document.getElementById("displayColor");
 colorDisplay.textContent = pickedColor;
+let messageDisplay = document.querySelector("#message");
+let heading = document.querySelector("#heading");
 
 for (let i = 0; i < squares.length; i++) {
   //loop through all the squares for giving them different colors
   squares[i].style.backgroundColor = colors[i];
+
   //add click listender to squares
   squares[i].addEventListener("click", function() {
     //grab color of cliked square
     let clickedColor = this.style.backgroundColor;
+
     //compare color to picked
     if (clickedColor === pickedColor) {
-      alert("Correct");
+      messageDisplay.textContent = "Correct";
+      changeColors(clickedColor);
+      heading.style.backgroundColor = clickedColor;
     } else {
-      alert("Wrong!");
+      this.style.backgroundColor = "#232323";
+      messageDisplay.textContent = "Try Again";
     }
   });
+}
+
+function changeColors(color) {
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = color;
+  }
+}
+
+function pickColor() {
+  let random = Math.floor(Math.random() * colors.length);
+  return colors[random];
+}
+
+function generateRandomColors(num) {
+  let arr = [];
+  for (let i = 0; i < num; i++) {
+    arr.push(randomColor());
+  }
+  return arr;
+}
+
+function randomColor() {
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+  return "rgb(" + r + ", " + g + ", " + b + ")";
 }
