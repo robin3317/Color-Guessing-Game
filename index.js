5b9795dc95330351adbb96c9445e77738cf458a1
robin3317 <abdurrahmanrobin47@gmail.com>
2018-05-05 14:06:11 +0600

    easy and hard mode is included

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-let colors = generateRandomColors(6);
+let numberOfSquares = 6;
+let colors = generateRandomColors(numberOfSquares);
 let squares = document.querySelectorAll(".square");
 let pickedColor = pickColor();
 let colorDisplay = document.getElementById("displayColor");
@@ -6,6 +7,38 @@ colorDisplay.textContent = pickedColor;
 let messageDisplay = document.querySelector("#message");
 let heading = document.querySelector("#heading");
 let resetButton = document.querySelector("#reset");
+let easyBtn = document.getElementById("easy");
+let hardBtn = document.getElementById("hard");
+
+easyBtn.addEventListener("click", function () {
+  easyBtn.classList.add("selected");
+  hardBtn.classList.remove("selected");
+  numberOfSquares = 3;
+  colors = generateRandomColors(numberOfSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for (let i = 0; i < squares.length; i++) {
+    if (colors[i]) {
+      squares[i].style.backgroundColor = colors[i];
+    } else {
+      squares[i].style.display = "none";
+    }
+
+  }
+});
+
+hardBtn.addEventListener("click", function () {
+  easyBtn.classList.remove("selected");
+  hardBtn.classList.add("selected");
+  numberOfSquares = 6;
+  colors = generateRandomColors(numberOfSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = colors[i];
+    squares[i].style.display = "block";
+  }
+});
 
 for (let i = 0; i < squares.length; i++) {
   //loop through all the squares for giving them different colors
@@ -57,7 +90,7 @@ function randomColor() {
 
 resetButton.addEventListener("click", function () {
   //generate all new colors
-  colors = generateRandomColors(6);
+  colors = generateRandomColors(numberOfSquares);
   //pick a new random color from array
   pickedColor = pickColor();
   //change colorDisplay to match picked color
@@ -68,4 +101,4 @@ resetButton.addEventListener("click", function () {
   }
   heading.style.backgroundColor = "#232323";
   resetButton.textContent = "New Color";
-})
+});
